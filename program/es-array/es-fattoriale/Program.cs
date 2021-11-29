b5e84744686037a77c9d26466cd359079aeb6fba
flaviofaraon <77169527+flaviofaraon@users.noreply.github.com>
2021-11-29 21:45:40 +0100

    Add files via upload
    c#

---
+++ program/es-array/es-fattoriale/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace es_fattoriale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inserisci un numero :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"il fattoriale di {n} è {Fattoriale(n)}");
+        }
+
+        static int Fattoriale(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Fattoriale ( n - 1);
+        }
+    }
+}
